2cf92d93de4fa62d6ad71790c6c7db3306b611b9
Philip Bordallo <git@philipbordallo.com>
2017-08-30 19:13:24 -0700

    Pull data out of repo and setup build process

--- data/build.js
+++ data/build.js
@@ -1,59 +1,20 @@
 /* eslint-disable no-console */
-import fs from 'fs';
 import path from 'path';
+import finder from './finder';
 
-import { nameSort } from '../src/utilities/sorts';
+const ROOT_PATH = path.resolve(__dirname, '..');
+const DATA_PATH = path.resolve(ROOT_PATH, 'data')
+const OPP_PATH = path.resolve(DATA_PATH, 'opportunities');
+const DIST_PATH = path.resolve(ROOT_PATH, 'dist');
+const DIST_DATA_PATH = path.resolve(DIST_PATH, 'data');
 
-const OPP_PATH = path.join(__dirname, 'opportunities');
-const ALL_FILE = path.join(__dirname, 'all.json');
-
-
-const Data = {
-	read(dirname) {
-		return new Promise((resolve, reject) => {
-			fs.readdir(dirname, (error, filenames) => {
-				if (error) reject(error);
-
-				const data = [];
-
-				filenames.forEach((filename) => {
-					const readFrom = path.join(dirname, filename);
-
-					if (filename !== '.DS_Store') {
-						const content = fs.readFileSync(readFrom, 'utf8');
-						data.push(JSON.parse(content));
-					}
-				});
-
-				resolve(data);
-			});
-		});
-	},
-
-	sort(data) {
-		return new Promise((resolve) => {
-			data.sort(nameSort);
-
-			resolve(data);
-		});
-	},
-
-	write(data, file) {
-		return new Promise((resolve, reject) => {
-			fs.writeFile(file, JSON.stringify(data), (error) => {
-				if (error) reject(error);
-
-				resolve(`  Finished => ${file}`);
-			});
-		});
-	}
-};
-
-Data.read(OPP_PATH)
-	.then(data => Data.sort(data))
-	.then(data => Data.write(data, ALL_FILE))
-	.then((data) => {
-		console.log(data);
+finder.read(OPP_PATH)
+	.then(data => finder.sort(data))
+	.then(data => finder.mkdir(DIST_PATH, data))
+	.then(data => finder.mkdir(DIST_DATA_PATH, data))
+	.then(data => finder.write(data, path.resolve(DIST_DATA_PATH, 'all.json')))
+	.then((results) => {
+		console.log(results);
 	})
 	.catch((error) => {
 		console.error('Error!', error);
--- spec/data/data_spec.js
+++ spec/data/data_spec.js
@@ -1,17 +1,28 @@
 import test from 'ava';
+import path from 'path'
+
 import { Validator } from 'jsonschema';
-
 import schema from './schema';
-import data from '../../data/all.json';
 
+import finder from '../../data/finder';
+
+
+const ROOT_PATH = path.resolve(__dirname, '..', '..');
+const OPP_PATH = path.resolve(ROOT_PATH, 'data', 'opportunities');
 
 const jsonSchema = new Validator();
 
-data.forEach(item => {
-	test(item.name, t => {
-		const schemaCheck = jsonSchema.validate(item, schema);
-		const message = schemaCheck.errors.map((error, index) => `${index + 1}. ${error.stack}`).join('\n');
+finder.read(OPP_PATH)
+	.then(data => {
+		data.forEach(item => {
+			test(item.name, t => {
+				const schemaCheck = jsonSchema.validate(item, schema);
+				const message = schemaCheck.errors.map((error, index) => `${index + 1}. ${error.stack}`).join('\n');
 
-		t.true(schemaCheck.valid, message);
+				t.true(schemaCheck.valid, message);
+			})
+		});
 	})
-});
+	.catch(error => {
+		console.error(error)
+	});
